b39828851a653c478de5ab5f44f25204a9913e16
Mikhails S <awarebayes@gmail.com>
2022-09-30 22:19:08 +0300

    add scene entity

--- src/render/scene/scene.cu
+++ src/render/scene/scene.cu
@@ -2,16 +2,22 @@
 // Created by dev on 8/28/22.
 //
 #include "scene.h"
+#include <imgui.h>
 
 std::shared_ptr<Camera> Scene::get_camera() {
 	return camera;
 }
-std::vector<ModelArgs> &Scene::get_models() {
-	return models;
+std::vector<ModelArgs> Scene::get_models() {
+	auto model_args = std::vector<ModelArgs>(models.size());
+	for (int i = 0; i < models.size(); i++) {
+		model_args[i] = models[i].to_args();
+	}
+	return model_args;
 }
+
 DrawCallArgs Scene::get_draw_call_args() {
 	DrawCallArgs args = {
-			.models = models,
+			.models = get_models(),
 			.base = {
 					.light_dir = light_dir,
 					.camera_pos = camera->position,
@@ -24,4 +30,37 @@ DrawCallArgs Scene::get_draw_call_args() {
 }
 float3 &Scene::get_light_dir() {
 	return light_dir;
+}
+void Scene::set_camera(const Camera &cam) {
+	camera = std::make_shared<Camera>(cam);
+}
+
+
+void Scene::display_menu() {
+	ImGui::Begin("Scene Controls");
+	ImGui::SliderFloat3("Light dir", &light_dir.x, -1, 1);
+	ImGui::End();
+
+	camera->display_menu();
+
+	ImGui::Begin("Models");
+	for (int i = 0; i < models.size(); i++)
+	{
+		if (ImGui::TreeNode((void*)(intptr_t)i, "Model %d", i))
+		{
+			ImGui::SliderFloat3("Position", &models[i].position.x, -10, 10);
+			ImGui::TreePop();
+		}
+	}
+	ImGui::End();
+}
+void Scene::add_model(const StoredModel &model) {
+	models.push_back(model);
+}
+StoredModel &Scene::get_model(int index) {
+	return models[index];
+}
+
+int  Scene::get_n_models() {
+	return models.size();
 }
--- src/render/scene/scene.h
+++ src/render/scene/scene.h
@@ -5,4 +5,26 @@
 #ifndef COURSE_RENDERER_SCENE_H
 #define COURSE_RENDERER_SCENE_H
 
+#include "../../camera/camera.h"
+#include "../../kernels/inc/render.cuh"
+class Scene {
+private:
+	std::vector<StoredModel> models{};
+	std::shared_ptr<Camera> camera{};
+	float3 light_dir{0, 0, 1.5};
+public:
+	Scene() = default;
+	void display_menu();
+	void add_model(const StoredModel &model);
+	StoredModel &get_model(int index);
+	std::shared_ptr<Camera> get_camera();
+	void set_camera(const Camera &cam);
+	std::vector<ModelArgs> get_models();
+	float3 &get_light_dir();
+	DrawCallArgs get_draw_call_args();
+	int get_n_models();
+};
+
+using SceneSingleton = SingletonCreator<Scene>;
+
 #endif//COURSE_RENDERER_SCENE_H
